d3b6a2165e189f0da47bad6eca8dbabc7613c336
franciscogaletti@yahoo.com <franciscogaletti@yahoo.com>
2019-10-18 17:53:50 -0500

    example commit1

--- IdentityCore/IdentityCore/Startup.cs
+++ IdentityCore/IdentityCore/Startup.cs
@@ -30,6 +30,8 @@ namespace IdentityCore
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
+            // this is a StartUP EXAMPLE
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
